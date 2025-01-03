d035871f07beda8fd1b266c4aa658ccdf34303d6
paikman8 <low.flag0610@fastmail.com>
2025-01-03 15:16:49 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1133877602730536960"; // your league ID
+export const leagueID = "1112942364496134144"; // your league ID
 export const leagueName = "Jefferson's Justice"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
